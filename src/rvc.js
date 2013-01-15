8806b97860b757fe5e7f8fb7aeeca73858d65fe1
Matthew West <mwest@illinois.edu>
2013-01-15 08:52:24 -0600

    add js for #rvc

---
+++ src/rvc.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+
+    var rvc_fd_c = new PrairieDraw("rvc-fd-c", function() {
+        this.setUnits(8, 8);
+
+        
+
+    });
+
+}); // end of document.ready()
