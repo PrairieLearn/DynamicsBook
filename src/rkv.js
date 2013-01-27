df11d91d40b878e4a9a7d961dd23a4503061cdb9
Matthew West <mwest@illinois.edu>
2013-01-27 08:55:03 -0600

    add #rkv-fr figure for polar basis movement

--- src/rkv.js
+++ src/rkv.js
@@ -359,4 +359,167 @@ $(document).ready(function() {
         }
     });
 
+    var rkv_fr_c = new PrairieDrawAnim("rkv-fr-c", function(t) {
+        this.setUnits(12, 8);
+
+        this.addOption("movement", "circle");
+        this.addOption("showLabels", true);
+        this.addOption("showPath", false);
+        this.addOption("showVelocity", false);
+        this.addOption("showAcceleration", false);
+        this.addOption("showVelDecomp", false);
+        this.addOption("showAccDecomp", false);
+
+        var O = $V([0, 0]);
+        var f;
+        if (this.getOption("movement") === "arc") {
+            f = function(t) {
+                t = -t;
+                t += 5;
+                return {
+                    "period": 2 * Math.PI / 0.5,
+                    "P": this.polarToRect($V([2 - 0.5 * Math.cos(0.5 * t) - 0.5 * Math.cos(t), Math.PI / 2 + 2.5 * Math.sin(0.5 * t)]))
+                };
+            };
+        } else if (this.getOption("movement") === "circle") {
+            f = function(t) {
+                return {
+                    "period": 2 * Math.PI / 0.5,
+                    "P": this.polarToRect($V([2.5, 0.5 * t]))
+                };
+            };
+        } else if (this.getOption("movement") === "varCircle") {
+            f = function(t) {
+                return {
+                    "period": 2 * Math.PI / 0.5,
+                    "P": this.polarToRect($V([2.5, -0.5 * t + 0.2 * Math.sin(t)]))
+                };
+            };
+        } else if (this.getOption("movement") === "ellipse") {
+            f = function(t) {
+                t += 3;
+                return {
+                    "period": 2 * Math.PI / 0.7,
+                    "P": $V([Math.cos(0.7 * t), 3 * Math.sin(0.7 * t)])
+                };
+            };
+        } else if (this.getOption("movement") === "trefoil") {
+            f = function(t) {
+                t += 4;
+                return {
+                    "period": 2 * Math.PI / 0.4,
+                    "P": $V([Math.cos(0.4 * t) - 2 * Math.cos(2 * 0.4 * t), Math.sin(0.4 * t) + 2 * Math.sin(2 * 0.4 * t)])
+                };
+            };
+        } else if (this.getOption("movement") === "eight") {
+            f = function(t) {
+                t += 2.5 * Math.PI;
+                return {
+                    "period": 2 * Math.PI / 0.5,
+                    "P": this.polarToRect($V([3 * Math.cos(0.5 * t), Math.sin(0.5 * t)]))
+                };
+            };
+        } else if (this.getOption("movement") === "comet") {
+            f = function(t) {
+                t += 1;
+                var T = 2 * Math.PI / 0.7; // period
+                var a = 2; // semi-major axis
+                var e = 0.5; // eccentricity
+                var b = a * Math.sqrt(1 - e*e); // semi-minor axis
+                var M = 2 * Math.PI * t / T; // mean anomaly
+                var E = M; // eccentric anomaly
+                // solve M = E - e * sin(E) for E with Newton's method
+                for (var i = 0; i < 5; i++) {
+                    E = E + (M - (E - e * Math.sin(E))) / (1 - e * Math.cos(E));
+                }
+                return {
+                "period": T,
+                "P": $V([a * (Math.cos(E) - e), b * Math.sin(E)])
+            };};
+        } else if (this.getOption("movement") === "pendulum") {
+            f = function(t) {
+                t -= 1.5;
+                return {
+                    "period": 2 * Math.PI / 0.6,
+                    "P": this.polarToRect($V([3, -Math.PI / 2 + Math.cos(0.6 * t)]))
+                };
+            };
+        }
+        f = f.bind(this);
+
+        var val = this.numDiff(f, t);
+        var period = val.period;
+        var r = val.P;
+        var v = val.diff.P;
+        var a = val.ddiff.P;
+
+        var O = $V([0, 0]);
+        var ei = $V([1, 0]);
+        var ej = $V([0, 1]);
+
+        var er = r.toUnitVector();
+        var et = er.rotate(Math.PI / 2, O);
+
+        var vr = this.orthProj(v, er);
+        var vt = this.orthProj(v, et);
+        var ar = this.orthProj(a, er);
+        var at = this.orthProj(a, et);
+
+        var label = this.getOption("showLabels") ? true : undefined;
+
+        if (this.getOption("showPath")) {
+            var n = 200;
+            var path = [], s;
+            for (var i = 0; i < n; i++) {
+                s = i / n * period;
+                path.push(f(s).P);
+            }
+            this.polyLine(path, true, false);
+        }
+        this.point(O);
+        this.text(O, $V([1, 1]), label && "TEX:$O$");
+        this.point(r);
+        this.labelIntersection(r, [O, r.add(er), r.add(et)], label && "TEX:$P$");
+        this.arrow(O, r, "position");
+        this.labelLine(O, r, $V([0, 1]), label && "TEX:$\\vec{r}$");
+        this.arrow(r, r.add(er));
+        this.arrow(r, r.add(et));
+        this.labelLine(r, r.add(er), $V([1, 1]), label && "TEX:$\\hat{e}_r$");
+        this.labelLine(r, r.add(et), $V([1, 1]), label && "TEX:$\\hat{e}_\\theta$");
+        if (this.getOption("showVelocity")) {
+            this.arrow(r, r.add(v), "velocity");
+            this.labelLine(r, r.add(v), $V([0, -1]), label && "TEX:$\\vec{v}$");
+        }
+        if (this.getOption("showAcceleration")) {
+            this.arrow(r, r.add(a), "acceleration");
+            this.labelLine(r, r.add(a), $V([1, 0]), label && "TEX:$\\vec{a}$");
+        }
+        if (this.getOption("showVelDecomp") && vr.modulus() > 1e-3) {
+            this.arrow(r, r.add(vr), "velocity");
+            this.labelLine(r, r.add(vr), $V([1, 1]), label && "TEX:$\\vec{v}_r$");
+        }
+        if (this.getOption("showVelDecomp") && vt.modulus() > 1e-3) {
+            this.arrow(r, r.add(vt), "velocity");
+            this.labelLine(r, r.add(vt), $V([1, 1]), label && "TEX:$\\vec{v}_\\theta$");
+        }
+        if (this.getOption("showAccDecomp") && ar.modulus() > 1e-3) {
+            this.arrow(r, r.add(ar), "acceleration");
+            this.labelLine(r, r.add(ar), $V([1, 1]), label && "TEX:$\\vec{a}_r$");
+        }
+        if (this.getOption("showAccDecomp") && at.modulus() > 1e-3) {
+            this.arrow(r, r.add(at), "acceleration");
+            this.labelLine(r, r.add(at), $V([1, 1]), label && "TEX:$\\vec{a}_\\theta$");
+        }
+
+    });
+
+    rkv_fr_c.registerOptionCallback("movement", function (value) {
+        rkv_fr_c.resetTime(false);
+        rkv_fr_c.resetOptionValue("showPath");
+        rkv_fr_c.resetOptionValue("showVelocity");
+        rkv_fr_c.resetOptionValue("showAcceleration");
+        rkv_fr_c.resetOptionValue("showVelDecomp");
+        rkv_fr_c.resetOptionValue("showAccDecomp");
+    });
+
 }); // end of document.ready()
