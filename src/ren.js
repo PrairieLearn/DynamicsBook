7b641ad9de14f8572f893a50b1f015ca38326224
Matthew West <mwest@illinois.edu>
2013-03-30 09:45:44 -0500

    finish momentum additions to #ren

--- src/ren.js
+++ src/ren.js
@@ -107,4 +107,68 @@ $(document).ready(function() {
     ren_ff_c.activateMouseTracking();
     ren_ff_c.activateAnimOnClick();
 
+    var ren_xl_c = new PrairieDraw("ren-xl-c", function() {
+
+        this.setUnits(6, 4);
+
+        this.addOption("showBases", false);
+
+        var len = 2;
+        var theta = Math.PI / 4;
+        var O = $V([0, 0]);
+        var rP = $V([0, -len]).rotate(theta, O);
+
+        this.translate($V([0, 0.5]));
+
+        this.ground($V([0, 0.5]), $V([0, -1]), 2);
+        this.pivot($V([0, 0.5]), O, 0.4);
+        this.rod(O, rP, 0.2);
+        this.arc(rP, 0.15, undefined, undefined, true);
+        this.point(O);
+        this.point(rP);
+        this.text(rP.add($V([0, -0.15])), $V([0, 1]), "TEX:$m$");
+        this.save();
+        this.setProp("shapeStrokePattern", "dashed");
+        this.line(O.add($V([0, -0.5])), O.add($V([0, -len])));
+        this.restore();
+        this.circleArrow(O, len * 0.6, -Math.PI / 2, -theta - 0.1, "angle", true);
+        this.labelCircleLine(O, len * 0.6, -Math.PI / 2, -theta - 0.1, $V([0, 1]), "TEX:$\\theta$", true);
+        this.arrow($V([-2, -0.4]), $V([-2, -1.4]), "acceleration");
+        this.labelLine($V([-2, -0.4]), $V([-2, -1.4]), $V([0, 1]), "TEX:$g$");
+
+        if (this.getOption("showBases")) {
+            var ei = $V([1, 0]);
+            var ej = $V([0, 1]);
+            var eR = $V([0, -1]).rotate(theta, O);
+            var eTheta = $V([1, 0]).rotate(theta, O);
+
+            this.arrow(O, O.add(ei));
+            this.arrow(O, O.add(ej));
+            this.labelLine(O, O.add(ei), $V([1, 0]), "TEX:$\\hat\\imath$");
+            this.labelLine(O, O.add(ej), $V([1, 0]), "TEX:$\\hat\\jmath$");
+
+            this.arrow(rP, rP.add(eR));
+            this.arrow(rP, rP.add(eTheta));
+            this.labelLine(rP, rP.add(eR), $V([1, 0]), "TEX:$\\hat{e}_r$");
+            this.labelLine(rP, rP.add(eTheta), $V([1, 0]), "TEX:$\\hat{e}_\\theta$");
+        }
+    });
+
+    var ren_xl_f = new PrairieDraw("ren-xl-f", function() {
+
+        this.setUnits(6, 4);
+
+        var theta = Math.PI / 4;
+        var O = $V([0, 0]);
+        var eR = $V([0, -1]).rotate(theta, O);
+
+        this.arc(O, 0.15, undefined, undefined, true);
+        this.point(O);
+        this.text(O.add($V([0.15, 0])), $V([-1, 0]), "TEX:$m$");
+        this.arrow(O, O.add($V([0, -1.5])), "force");
+        this.labelLine(O, O.add($V([0, -1.5])), $V([1, 0]), "TEX:$mg$");
+        this.arrow(O, O.add($V([0, 1.5]).rotate(theta, O)), "force");
+        this.labelLine(O, O.add($V([0, 1.5]).rotate(theta, O)), $V([1, 0]), "TEX:$T$");
+    });
+
 }); // end of document.ready()
